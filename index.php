d4fc8d1bb020be5c5c2a9e419a829e1d9a563c95
Shah Lakshya Pradhyuman <shahlakshya1988@gmail.com>
2020-01-05 20:03:31 +0530

    7. Custom Navigation Style Using CSS3

--- index.php
+++ index.php
@@ -4,9 +4,34 @@
     <meta charset="UTF-8">
     <meta name="viewport" content="width=device-width, initial-scale=1.0, shrink-to-fit=no">
     <meta http-equiv="X-UA-Compatible" content="ie=edge">
-    <title>Document</title>
+    <title>Udemy Signup &amp; Login Form</title>
+    <link rel="stylesheet" href="./assets/css/bootstrap.min.css" />
+    <link rel="stylesheet" href="./assets/font-awesome/css/font-awesome.min.css" />
+    <link rel="stylesheet" href="./assets/css/styles.css" />    
+    <script type="text/javascript" src="./assets/js/jquery.min.js" ></script>
 </head>
 <body>
-    
+    <!-- ==== NAVBAR ==== -->
+    <nav class="navbar navbar-expand-sm navbar-light bg-light custom-nav">
+        <!-- div.container -->
+        <div class="container">
+            <a href="#" class="navbar navbar-brand">Udemy Signup &amp; Login Form</a>
+            <button type="button" class="navbar-toggler" data-target="#mynav" data-toggle="collapse">
+                <span class="navbar-toggler-icon"></span>
+            </button>
+            <!-- div.collapse .navbar-collapse-->
+            <div class="collapse navbar-collapse" id="mynav">
+                <ul class="navbar-nav ml-auto">
+                    <li class="nav-item">
+                        <a href="#" class="nav-link">Home</a>
+                    </li>
+                </ul>
+            </div>
+            <!-- div.collapse .navbar-collapse-->
+        </div>
+        <!-- div.container -->
+    </nav>
+    <!-- ==== NAVBAR ==== -->
+    <script type="text/javascript" src="./assets/js/bootstrap.bundle.min.js" ></script>
 </body>
 </html>
