54366387ee142b7360e1c96ab9dc37534f519ff9
Shah Lakshya Pradhyuman <shahlakshya1988@gmail.com>
2020-01-06 08:00:45 +0530

    12. Apply jQuery Effects On Singup & Login Form

---
+++ assets/js/simple.js
@@ -0,0 +1,10 @@
+$(document).ready(function(){
+    $("#login").click(function(){
+        $(".signup-cover").hide();
+        $(".login-cover").show();
+    });
+    $("#signup").click(function(){
+        $(".signup-cover").show();
+        $(".login-cover").hide();
+    });
+});
--- index.php
+++ index.php
@@ -153,5 +153,6 @@
     </div>
 	<!-- div.container -->
     <script type="text/javascript" src="./assets/js/bootstrap.bundle.min.js" ></script>
+    <script type="text/javascript" src="./assets/js/simple.js"></script>
 </body>
 </html>
