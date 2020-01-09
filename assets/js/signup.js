280bd5c0bac90f625d94bab9cfb8ecd343a42c34
Lakshya Shah <shahlakshya1988@gmail.com>
2020-01-09 11:15:57 +0530

    23. Name Field Validation Part2

--- assets/js/signup.js
+++ assets/js/signup.js
@@ -4,19 +4,29 @@ $(document).ready(function(){
     var email = "";
     var password = "";
     var confirm = "";
-
+    var name_reg = /^[a-z ]+$/i;
+    /****  ==== NAME VALIDATION ==== */
     $("#name").focusout(function(){
         // alert("input name");
         var store = $("#name").val().trim();
         if(store.length == 0 || store.length==""){
+            $(this).removeClass("border-green");
             $(this).addClass("border-red");
             $(".name-error").html("Name is required");
             name="";
+        }else if(!name_reg.test(store)){ 
+            $(this).removeClass("border-green");
+            $(this).addClass("border-red");
+            $(".name-error").html("Numbers, Special Chars Are Not Allowed");
+            name="";
+
         }else{
             // alert(store);
             $(this).removeClass("border-red");
+            $(this).addClass("border-green");
             $(".name-error").html("");
             name=store;
         }
     });
+    /****  ==== NAME VALIDATION ==== */
 });
